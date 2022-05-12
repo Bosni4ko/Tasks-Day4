a3cf94eac077558bc211491a68f6a7f1ffc423a8
Bosni4ko <stefanbosnyak9@gmail.com>
2022-05-12 10:23:50 +0300

    Task1

--- Task1.java
+++ Task1.java
@@ -1,3 +1,17 @@
-public class Task1 {
+import java.util.ArrayList;
 
+public class Task1 {
+     public static void main(String[] args) {
+        String[] arr = {"purple","magenta","pink","blue","black","red","crimson"};
+        ArrayList<String> colors = new ArrayList<String>();
+
+        for(String i : arr){
+            colors.add(i);
+        }
+        colors.remove(2);
+        colors.add("cyan");
+        colors.add(0,"orange");
+
+         colors.forEach(System.out::println);
+    }
 }
